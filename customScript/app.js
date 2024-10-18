80a8e350b64a4257175ecea8602c5cff0bb29813
estymate <gl@bulme.at>
2024-10-18 19:37:58 +0200

    improved style

---
+++ customScript/app.js
@@ -0,0 +1,19 @@
+//TODO: Update version number
+let newParam = "v=2024-10-18";
+
+document.querySelector("nav a.gitbook-link").remove();
+let links = document.querySelectorAll("nav a");
+
+links.forEach(linkElement => {
+    let currentHref = linkElement.href;
+
+    if (currentHref.includes('?')) {
+        query = '&' + newParam;
+    }
+    else {
+        query = '?' + newParam;
+    }
+
+    currentHref += query;
+    linkElement.href = currentHref;
+});
