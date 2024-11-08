13735f1a46398839317530c334f0a7fbd73b52f8
estymate <gl@bulme.at>
2024-11-08 08:39:33 +0100

    deactivated script

--- customScript/app.js
+++ customScript/app.js
@@ -1,19 +1,1 @@
 //TODO: Update version number
-let newParam = "v=2024-11-08";
-
-document.querySelector("nav a.gitbook-link").remove();
-let links = document.querySelectorAll("nav a");
-
-links.forEach(linkElement => {
-    let currentHref = linkElement.href;
-
-    if (currentHref.includes('?')) {
-        query = '&' + newParam;
-    }
-    else {
-        query = '?' + newParam;
-    }
-
-    currentHref += query;
-    linkElement.href = currentHref;
-});
