5b57cd44e59a9f77b1d463c5fe90dde939556ffa
estymate <gl@bulme.at>
2024-11-09 22:04:41 +0100

    fixed page nav

--- _book/gitbook/gitbook-plugin-add-js-css/99a0fea7d4f6d405c09042a90155e9cf-app.js
+++ _book/gitbook/gitbook-plugin-add-js-css/99a0fea7d4f6d405c09042a90155e9cf-app.js
@@ -1,6 +1,7 @@
 //TODO: Update version number
 document.querySelector("nav a.gitbook-link").remove();
 
-$('td:contains("false")').addClass("bg-false"); 
-
-$('td:contains("true")').addClass("bg-true");
+gitbook.events.on('page.change', function () {
+    $('td:contains("false")').addClass("bg-false"); 
+    $('td:contains("true")').addClass("bg-true");
+});
