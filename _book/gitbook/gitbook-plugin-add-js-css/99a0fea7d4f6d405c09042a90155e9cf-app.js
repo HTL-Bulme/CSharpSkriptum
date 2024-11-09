e3f056648626551e32425516121f51e7217f88ea
estymate <gl@bulme.at>
2024-11-09 21:15:48 +0100

    Added Color

--- _book/gitbook/gitbook-plugin-add-js-css/99a0fea7d4f6d405c09042a90155e9cf-app.js
+++ _book/gitbook/gitbook-plugin-add-js-css/99a0fea7d4f6d405c09042a90155e9cf-app.js
@@ -1,2 +1,6 @@
 //TODO: Update version number
 document.querySelector("nav a.gitbook-link").remove();
+
+$('td:contains("false")').addClass("bg-false"); 
+
+$('td:contains("true")').addClass("bg-true");
